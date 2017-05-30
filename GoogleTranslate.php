33f355b87e493af20a82e48c9eee0acb1e394c68
Mohamamd Mahdi <ebrahimi.mahdi593@gmail.com>
2017-05-30 19:34:58 +0430

    Update GoogleTranslate.php

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -1,5 +1,11 @@
 <?php
 
+// __  __       _                                         _    _               _
+//|  \/  | ___ | |__   __ _ _ __ ___  _ __ ___   __ _  __| |  / \   _ __ __ _ | | __
+//| |\/| |/ _ \| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _` |/ _` | / _ \ | '__/ _` || |/ /
+//| |  | | (_) | | | | (_| | | | | | | | | | | | (_| | (_| |/ ___ \| | | (_| ||   <
+//|_|  |_|\___/|_| |_|\__,_|_| |_| |_|_| |_| |_|\__,_|\__,_/_/   \_\_|  \__,_||_|\_\
+
 function translate($text, $to) {
     $json_array = file_get_contents('https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&sl=auto&tl=' . $to . '&dt=t&q=' . $text);
     $jdat = json_decode($json_array);
