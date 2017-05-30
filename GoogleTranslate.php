357d07d19c0cffba311652a7e03f2a4c31fc1bff
Mohamamd Mahdi <ebrahimi.mahdi593@gmail.com>
2017-05-30 19:36:46 +0430

    Updating Class

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -6,28 +6,29 @@
 //| |  | | (_) | | | | (_| | | | | | | | | | | | (_| | (_| |/ ___ \| | | (_| ||   <
 //|_|  |_|\___/|_| |_|\__,_|_| |_| |_|_| |_| |_|\__,_|\__,_/_/   \_\_|  \__,_||_|\_\
 
-function translate($text, $to) {
-    $json_array = file_get_contents('https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&sl=auto&tl=' . $to . '&dt=t&q=' . $text);
-    $jdat = json_decode($json_array);
-    return $jdat[0][0][0];
-}
-
-function get_lang($input){
-    $languages = array(
-        'en'=>'English',
-        'fa'=>'Persian',
-        'fr'=>'French',
-        'ar'=>'Arabic',
-        'ru'=>'Russian',
-        'ja'=>'Japanese',
-        'zh'=>'Chinese',
-        'de'=>'German',
-        'es'=>'Spanish'
-    );
-    if($languages[$input]){
-        return $languages[$input];
-    }else{
-        return $input;
+class GoogleTranslate{
+    public function translate($text, $to) {
+        $json_array = file_get_contents('https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&sl=auto&tl=' . $to . '&dt=t&q=' . $text);
+        $jdat = json_decode($json_array);
+        return $jdat[0][0][0];
+    }
+    public function get_lang($input){
+        $languages = array(
+            'en'=>'English',
+            'fa'=>'Persian',
+            'fr'=>'French',
+            'ar'=>'Arabic',
+            'ru'=>'Russian',
+            'ja'=>'Japanese',
+            'zh'=>'Chinese',
+            'de'=>'German',
+            'es'=>'Spanish'
+        );
+        if($languages[$input]){
+            return $languages[$input];
+        }else{
+            return $input;
+        }
     }
 }
 
